2949f8f2415f02dcfa20871b7aa150ad28bcebc7
williamlevan <wlevan@stlcitysc.com>
2023-12-12 12:03:41 -0600

    create a cube and an arrow

---
+++ arrow.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+const renderer = new THREE.WebGLRenderer(); 
+renderer.setSize(window.innerWidth, window.innerHeight); 
+document.body.appendChild(renderer.domElement); 
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500); 
+camera.position.set(0, 0, 100); 
+camera.lookAt(0, 0, 0); 
+const scene = new THREE.Scene();
+//create a blue LineBasicMaterial 
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = []; 
+points.push( new THREE.Vector3( - 10, 0, 0 ) ); 
+points.push( new THREE.Vector3( 0, 10, 0 ) ); 
+points.push( new THREE.Vector3( 10, 0, 0 ) ); 
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+
+if ( WebGL.isWebGLAvailable() ) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
+
+function animate() {
+    renderer.render(scene, camera);
+}
